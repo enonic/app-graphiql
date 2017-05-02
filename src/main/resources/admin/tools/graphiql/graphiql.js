2601908a58de0dc9b920914d19bbc509d259b03d
Glenn Ricaud <gri@enonic.com>
2017-05-02 14:27:20 +0200

    GraphiQL - Add application header (#1)

--- src/main/resources/admin/tools/graphiql/graphiql.js
+++ src/main/resources/admin/tools/graphiql/graphiql.js
@@ -1,13 +1,14 @@
 var portalLib = require('/lib/xp/portal');
 var mustacheLib = require('/lib/xp/mustache');
 
-exports.get = function () {
+exports.get = function (req) {
     var view = resolve('./graphiql.html');
     var assetsUrl = portalLib.assetUrl({path: ""});
     var params = {
         graphiQLAssetsUrl: assetsUrl + '/graphiql',
         toolAssetsUrl: assetsUrl + '/tool',
-        graphQlServiceUrl: portalLib.serviceUrl({
+        query: req.params.query,
+        graphQlServiceUrl: req.params.location || portalLib.serviceUrl({
             service: 'graphql',
             application: 'com.enonic.app.myapp'
         })
